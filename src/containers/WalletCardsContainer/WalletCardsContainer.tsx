df29963c12e2527685824c9a3651b76cfae12e48
kitchun0402 <hkc930402@gmail.com>
2024-01-22 20:32:11 +0800

    feat: fetch balances

--- src/containers/WalletCardsContainer/WalletCardsContainer.tsx
+++ src/containers/WalletCardsContainer/WalletCardsContainer.tsx
@@ -1,17 +1,47 @@
+import { useEffect, useState } from 'react'
 import WalletCard from '../../components/cards/WalletCard/WalletCard'
+import { useAppSelector } from '../../hooks/redux'
+import { getWalletBalances } from '../../utils/getWalletBalances'
 import { Container } from './WalletCardsContainer.elements'
 
 function WalletCardsContainer() {
-  const walletList: Array<{ address: string; balance: string }> = []
+  const walletList = useAppSelector((state) => state.user.wallets)
+  const { readOnlyProvider, currentNetworkId } = useAppSelector(
+    (state) => state.network,
+  )
+  const [walletBalances, setWalletBalances] = useState<string[]>([])
+
+  // get wallet balances
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const balances = await getWalletBalances(
+          currentNetworkId,
+          readOnlyProvider,
+          walletList.map((wallet) => wallet.address),
+        )
+        setWalletBalances(balances)
+      } catch (error) {
+        // should handle errors
+        console.log(error)
+      }
+    }
+    void getData()
+
+    return () => {
+      return setWalletBalances([])
+    }
+  }, [currentNetworkId, readOnlyProvider, walletList])
+
   return (
     <Container>
       {walletList.length > 0 ? (
-        walletList.map((wallet) => {
+        walletList.map((wallet, index) => {
           return (
             <WalletCard
               key={wallet.address}
               address={wallet.address}
-              balance={wallet.balance}
+              balance={walletBalances[index] || '-'}
             />
           )
         })
