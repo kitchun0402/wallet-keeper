0f4c69e7c9183137d4bb229ffc84ca858ff38a61
kitchun0402 <hkc930402@gmail.com>
2024-01-22 20:54:36 +0800

    test: wrap user actions with act

--- src/components/buttons/AddNewWalletButton/__test__/AddNewWalletButton.test.tsx
+++ src/components/buttons/AddNewWalletButton/__test__/AddNewWalletButton.test.tsx
@@ -3,40 +3,52 @@ import { act, render, screen } from '../../../../testUtils'
 import AddNewWalletButton from '../AddNewWalletButton'
 
 describe('AddNewWalletButton', () => {
-  // it('should render the button without errors', () => {
-  //   render(<AddNewWalletButton />)
-  //   const button = screen.getByText('Add new wallet')
-  //   expect(button).toBeInTheDocument()
-  // })
+  it('should render the button without errors', () => {
+    render(<AddNewWalletButton />)
+    const button = screen.getByText('Add new wallet')
+    expect(button).toBeInTheDocument()
+  })
 
-  // it('should open the modal after clicking the addNewWalletButton', () => {
-  //   render(<AddNewWalletButton />)
-  //   const button = screen.getByText('Add new wallet')
-  //   userEvent.click(button)
-  //   const modalLabelText = screen.getByText('Enter your password')
-  //   expect(modalLabelText).toBeInTheDocument()
-  // })
-  // it('should close the modal after clicking the close button', () => {
-  //   render(<AddNewWalletButton />)
-  //   const button = screen.getByText('Add new wallet')
-  //   userEvent.click(button)
-  //   const cancelButton = screen.getByText('Cancel')
-  //   userEvent.click(cancelButton)
-  //   const modalLabelText = screen.queryByText('Enter your password')
-  //   expect(modalLabelText).not.toBeInTheDocument()
-  // })
-  // it('should close the modal after clicking the confirm button with the valid input', () => {
-  //   render(<AddNewWalletButton />)
-  //   const button = screen.getByText('Add new wallet')
-  //   userEvent.click(button)
-  //   const passwordInput = screen.getByTestId('password-input')
-  //   userEvent.type(passwordInput, '123456')
-  //   const confirmButton = screen.getByText('Confirm')
-  //   userEvent.click(confirmButton)
-  //   // The modal should be closed
-  //   const modalLabelText = screen.queryByText('Enter your password')
-  //   expect(modalLabelText).not.toBeInTheDocument()
-  // })
+  it('should open the modal after clicking the addNewWalletButton', () => {
+    render(<AddNewWalletButton />)
+    const button = screen.getByText('Add new wallet')
+    act(() => {
+      userEvent.click(button)
+    })
+    const modalLabelText = screen.getByText('Enter your password')
+    expect(modalLabelText).toBeInTheDocument()
+  })
+  it('should close the modal after clicking the close button', () => {
+    render(<AddNewWalletButton />)
+    const button = screen.getByText('Add new wallet')
+    act(() => {
+      userEvent.click(button)
+    })
+    const cancelButton = screen.getByText('Cancel')
+    act(() => {
+      userEvent.click(cancelButton)
+    })
+    const modalLabelText = screen.queryByText('Enter your password')
+    expect(modalLabelText).not.toBeInTheDocument()
+  })
+  it('should close the modal after clicking the confirm button with the valid input', () => {
+    render(<AddNewWalletButton />)
+    const button = screen.getByText('Add new wallet')
+    act(() => {
+      userEvent.click(button)
+    })
+    const passwordInput = screen.getByTestId('password-input')
+    act(() => {
+      userEvent.type(passwordInput, '123456')
+    })
+    const confirmButton = screen.getByText('Confirm')
+    act(() => {
+      userEvent.click(confirmButton)
+    })
+    // The modal should be closed
+    const modalLabelText = screen.queryByText('Enter your password')
+    expect(modalLabelText).not.toBeInTheDocument()
+  })
   it('should show the error message when the input is empty', () => {
     render(<AddNewWalletButton />)
     const button = screen.getByText('Add new wallet')
