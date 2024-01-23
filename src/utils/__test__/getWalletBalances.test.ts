6b01bb78bade5928e7bd4bea85699051ad4bb206
kitchun0402 <hkc930402@gmail.com>
2024-01-23 17:55:57 +0800

    test: add test cases for getWalletBalances

--- src/utils/__test__/getWalletBalances.test.ts
+++ src/utils/__test__/getWalletBalances.test.ts
@@ -1,23 +1,31 @@
-import { ethers } from 'ethers'
+import { ethers, utils } from 'ethers'
 import { availableNetworks } from '../../configs/network'
 import { NetworkId } from '../../types/network'
 import { getWalletBalances } from '../getWalletBalances'
-
 describe('getWalletBalances', () => {
   it('should return an array of wallet balances', async () => {
     const networkId = NetworkId.AVALANCHE_FUJI
-
     const provider = new ethers.providers.JsonRpcProvider(
       availableNetworks[networkId].rpcUrl,
     )
+    const balance = '100.0'
     const addresses = [
       '0x90abD5A18420474Af68C9DEEaB2786be696473A3',
       '0x0D7af8f6a1Ce70Ec38E5b6605ddaF8308636DB1a',
     ]
+    const mockGetBalance = jest.fn()
+    mockGetBalance.mockReturnValue(
+      Promise.resolve(
+        utils.parseUnits(balance, availableNetworks[networkId].decimals),
+      ),
+    )
+    jest
+      .spyOn(ethers.providers.JsonRpcProvider.prototype, 'getBalance')
+      .mockImplementation(mockGetBalance)
 
     const balances = await getWalletBalances(networkId, provider, addresses)
 
     expect(balances).toHaveLength(addresses.length)
-    expect(balances[0]).toMatch(/^\d+(\.\d+)?$/)
+    expect(balances[0]).toBe(balance)
   })
 })
